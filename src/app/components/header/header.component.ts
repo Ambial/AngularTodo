b2f6d5b9dc7d677a4cb5b89be7c71e5205541519
Simon Koerner <simon.koerner@nexenio.com>
2021-11-14 23:51:07 +0100

    Use routing, add footer, add about page

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UiService } from "src/app/services/ui.service";
 import { Subscription } from "rxjs";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -13,7 +14,9 @@ export class HeaderComponent implements OnInit {
   showAddTask:boolean = true;
   subscription:Subscription;
 
-  constructor(private uiService:UiService) { 
+  constructor(
+    private uiService:UiService,
+    private router:Router) { 
     this.subscription = this.uiService
       .onToggle()
       .subscribe(val => this.showAddTask = val)
@@ -24,7 +27,11 @@ export class HeaderComponent implements OnInit {
 
   toggleAddTask(): void {
     this.uiService.toggleAddTask();
+  }
 
+  isOnHomePage():boolean {
+    console.log(this.router.url)
+    return this.router.url === "/";
   }
 
 }
